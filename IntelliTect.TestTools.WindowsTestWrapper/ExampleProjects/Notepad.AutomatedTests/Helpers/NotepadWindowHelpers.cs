c269beb2601cc6182c90f494436157985cf9de77
mcc3453 <mike.curn@avistacorp.com>
2016-12-21 12:02:34 -0800

    Changes for properly formatting a unique filename

--- IntelliTect.TestTools.WindowsTestWrapper/ExampleProjects/Notepad.AutomatedTests/Helpers/NotepadWindowHelpers.cs
+++ IntelliTect.TestTools.WindowsTestWrapper/ExampleProjects/Notepad.AutomatedTests/Helpers/NotepadWindowHelpers.cs
@@ -10,15 +10,15 @@ namespace Notepad.AutomatedTests.Helpers
         private readonly NotepadWindow _NotepadWindow = new NotepadWindow();
         private readonly SaveAsWindow _SaveAsWindow = new SaveAsWindow();
 
-        //TODO: Is there a better way to handle saving a document for a test?
+        //TODO: Is there a better way to handle saving a document for a test? E.G. a randomly generated filename in the specified location?
+        //TODO: Assess if part of this should be pulled out and put into the wrapper Utilities.cs class
         public void SaveDocument(string location, string filename)
         {
             //Check if doc with same name already exists in location before saving
             string formattedPath = Path.Combine(location, filename);
             if (File.Exists(formattedPath))
             {
-                //TODO: Fix this so it appends to the name BEFORE the extension, per Kevin's suggestion
-                filename += "1";
+                filename = GetUniqueFileName( formattedPath );
             }
             Mouse.Click();
             Mouse.Click(_NotepadWindow.Save);
@@ -26,6 +26,22 @@ namespace Notepad.AutomatedTests.Helpers
             _SaveAsWindow.SaveAsBox.EditableItem = filename;
             _SaveAsWindow.SaveButton.SetFocus();
             SendKeys.SendWait("{ENTER}");
+        }
+
+        public static string GetUniqueFileName(string filePath)
+        {
+            string uniqueFileName = filePath;
+            for (int i = 0; File.Exists(uniqueFileName); uniqueFileName = AppendFileName(filePath, $" ({++i})"))
+            { }
+            return uniqueFileName;
+        }
+
+        public static string AppendFileName(string filePath, string toAppend)
+        {
+            var dir = Path.GetDirectoryName(filePath);
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            var extension = Path.GetExtension(filePath);
+            return Path.Combine(dir ?? "", string.Concat(fileName, toAppend, extension));
         }
     }
 }
