a00132a8d13ea00fb3afc0387fc6f4704f695cff
Mark Michaelis <mark@IntelliTect.com>
2018-09-25 08:07:22 -0700

    Added a build task that triggers on 'dotnet pack'; Added suppport to IncludeSymbols and IncludeSource; Added a test script that verifies the Assembly is included in the package (it wasn't for a while); Added a copy task that moves the nupkg files to the bin\ folder and identifies to git the intent to add them.

--- Test-NuGetPackage.ps1
+++ Test-NuGetPackage.ps1
@@ -1,26 +1,36 @@
 [CmdletBinding()]
 param(
-    [ValidateScript({Test-Path $_})][string]$path
+    [ValidateScript({Test-Path $_})][Parameter(Mandatory)][Alias('NugetPackagePath')][string]$Path,
+    [string]$AssemblyName
 )
 
 Function Test-NuGetPackage {
     [CmdletBinding()]
     param(
-        [ValidateScript({Test-Path $_})][string]$path
+        [ValidateScript({Test-Path $_})][string]$Path,
+        [string]$AssemblyName
     )
-
-    $path = Resolve-Path $path
+    if(-not $AssemblyName) {
+        Write-Warning '$AssemblyName parameter is empty.'
+    }
+    $Path = Resolve-Path $Path
     [bool]$assemblyFound = $false
-    Read-Archive -Path $path | %{
-        if($_.Path -like 'lib\*.dll') {
+    Read-Archive -Path $Path | %{
+        # Look for the specific assembly within the Nuget package if $AssemblyName is provided.
+        if($_.Path -like "lib\*$AssemblyName.dll") {
             $assemblyFound = $true
+            Write-Information -
         }
     }
     if(-not $assemblyFound) {
-        throw "No assembly found in 'lib' directory."
+        Write-Error "No assembly found in 'lib' directory."
+        Write-Output $false
     }
-
-
+    else {
+        Write-Output $true
+    }
 }
 
-Test-NuGetPackage @PSBoundParameters
+if($PSBoundParameters.Count -gt 0) {
+    Test-NuGetPackage @PSBoundParameters
+}
