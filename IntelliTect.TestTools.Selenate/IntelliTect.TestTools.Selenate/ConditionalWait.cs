69243ffa74bf19a9a9883aa560a051a17877ceb2
Michael Curn <mike.curn@IntelliTect.com>
2018-10-10 15:37:22 -0700

    Moar comments

--- IntelliTect.TestTools.Selenate/IntelliTect.TestTools.Selenate/ConditionalWait.cs
+++ IntelliTect.TestTools.Selenate/IntelliTect.TestTools.Selenate/ConditionalWait.cs
@@ -14,7 +14,7 @@ namespace IntelliTect.TestTools.Selenate
         /// <param name="func">Function to check for valid evaluation</param>
         /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
         /// <param name="exceptionsToIgnore">A list of exceptions to ignore when attempting to evaluate the function</param>
-        /// <returns></returns>
+        /// <returns>An async task that can return a value of type TResult</returns>
         public Task<TResult> WaitFor<TResult>(Func<TResult> func, TimeSpan timeToWait, params Type[] exceptionsToIgnore)
         {
             if(exceptionsToIgnore.Any(t => t.GetType() != typeof(Exception)))
@@ -30,7 +30,7 @@ namespace IntelliTect.TestTools.Selenate
         /// <param name="action">Function to check for valid evaluation</param>
         /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
         /// <param name="exceptionsToIgnore">A list of exceptions to ignore when attempting to evaluate the function</param>
-        /// <returns></returns>
+        /// <returns>An async task for the operation</returns>
         public Task WaitFor(Action action, TimeSpan timeToWait, params Type[] exceptionsToIgnore)
         {
             if (exceptionsToIgnore.Any(t => t.GetType() != typeof(Exception)))
@@ -40,17 +40,42 @@ namespace IntelliTect.TestTools.Selenate
             return ExecuteWait(action, timeToWait, exceptionsToIgnore);
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TResult">Return type of the function to evaluate</typeparam>
+        /// <param name="func">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task that can return a value of type TResult</returns>
         public Task<TResult> WaitFor<TException, TResult>(Func<TResult> func, TimeSpan timeToWait)
             where TException : Exception
         {
             return ExecuteWait(func, timeToWait, typeof(TException));
         }
+
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <param name="action">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task for the operation</returns>
         public Task WaitFor<TException>(Action action, TimeSpan timeToWait)
             where TException : Exception
         {
             return ExecuteWait(action, timeToWait, typeof(TException));
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException1">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException2">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TResult">Return type of the function to evaluate</typeparam>
+        /// <param name="func">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task that can return a value of type TResult</returns>
         public Task<TResult> WaitFor<TException1, TException2, TResult>(Func<TResult> func, TimeSpan timeToWait)
             where TException1 : Exception
             where TException2 : Exception
@@ -58,6 +83,14 @@ namespace IntelliTect.TestTools.Selenate
             return ExecuteWait(func, timeToWait, typeof(TException1), typeof(TException2));
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException1">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException2">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <param name="action">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task for the operation</returns>
         public Task WaitFor<TException1, TException2>(Action action, TimeSpan timeToWait)
             where TException1 : Exception
             where TException2 : Exception
@@ -65,6 +98,16 @@ namespace IntelliTect.TestTools.Selenate
             return ExecuteWait(action, timeToWait, typeof(TException1), typeof(TException2));
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException1">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException2">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException3">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TResult">Return type of the function to evaluate</typeparam>
+        /// <param name="func">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task that can return a value of type TResult</returns>
         public Task<TResult> WaitFor<TException1, TException2, TException3, TResult>(Func<TResult> func, TimeSpan timeToWait)
             where TException1 : Exception
             where TException2 : Exception
@@ -73,6 +116,15 @@ namespace IntelliTect.TestTools.Selenate
             return ExecuteWait(func, timeToWait, typeof(TException1), typeof(TException2), typeof(TException3));
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException1">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException2">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException3">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <param name="action">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task for the operation</returns>
         public Task WaitFor<TException1, TException2, TException3>(Action action, TimeSpan timeToWait)
             where TException1 : Exception
             where TException2 : Exception
@@ -81,6 +133,17 @@ namespace IntelliTect.TestTools.Selenate
             return ExecuteWait(action, timeToWait, typeof(TException1), typeof(TException2), typeof(TException3));
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException1">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException2">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException3">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException4">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TResult">Return type of the function to evaluate</typeparam>
+        /// <param name="func">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task that can return a value of type TResult</returns>
         public Task<TResult> WaitFor<TException1, TException2, TException3, TException4, TResult>(Func<TResult> func, TimeSpan timeToWait)
             where TException1 : Exception
             where TException2 : Exception
@@ -90,6 +153,16 @@ namespace IntelliTect.TestTools.Selenate
             return ExecuteWait(func, timeToWait, typeof(TException1), typeof(TException2), typeof(TException3), typeof(TException4));
         }
 
+        /// <summary>
+        /// Repeatedly checks for a condition with void return type until it is satisifed or a timeout is reached
+        /// </summary>
+        /// <typeparam name="TException1">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException2">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException3">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <typeparam name="TException4">An exception type to ignore when attempting to evaluate the function</typeparam>
+        /// <param name="action">Function to check for valid evaluation</param>
+        /// <param name="timeToWait">Time to try evaluating the given function until an exception is thrown</param>
+        /// <returns>An async task for the operation</returns>
         public Task WaitFor<TException1, TException2, TException3, TException4>(Action action, TimeSpan timeToWait)
             where TException1 : Exception
             where TException2 : Exception
