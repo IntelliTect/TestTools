faad0cea1f8caec0639f68f6d8a82809804db298
Michael <mjcurn@gmail.com>
2018-10-15 08:18:39 -0700

    Adding another unit test, refactoring existing ones to better conform to expected xUnit pattern

--- IntelliTect.TestTools.Selenate/IntelliTect.TestTools.Selenate.Tests/ConditionalWaitTests.cs
+++ IntelliTect.TestTools.Selenate/IntelliTect.TestTools.Selenate.Tests/ConditionalWaitTests.cs
@@ -6,19 +6,26 @@ namespace IntelliTect.TestTools.Selenate.Tests
     public class ConditionalWaitTests
     {
         [Fact]
-        public void CheckActionParamsForTypeChecking()
+        public async void CheckActionParamsForTypeChecking()
         {
             ConditionalWait wait = new ConditionalWait();
-            Exception ex = Assert.Throws<ArgumentException>(() => wait.WaitFor(TestVoidDelegate, TimeSpan.FromSeconds(1), typeof(string)).GetAwaiter().GetResult());
+            Exception ex = await Assert.ThrowsAsync<ArgumentException>(() => wait.WaitFor(TestVoidDelegate, TimeSpan.FromSeconds(1), typeof(string)));
             Assert.Equal("Invalid type passed into exceptionsToIgnore paramter. Must be of type Exception.", ex.Message);
         }
 
         [Fact]
-        public void CheckFuncParamsForTypeChecking()
+        public async void CheckFuncParamsForTypeChecking()
         {
             ConditionalWait wait = new ConditionalWait();
-            Exception ex = Assert.Throws<ArgumentException>(() => wait.WaitFor(TestReturnDelegate, TimeSpan.FromSeconds(1), typeof(string)).GetAwaiter().GetResult());
+            Exception ex = await Assert.ThrowsAsync<ArgumentException>(() => wait.WaitFor(TestReturnDelegate, TimeSpan.FromSeconds(1), typeof(string)));
             Assert.Equal("Invalid type passed into exceptionsToIgnore paramter. Must be of type Exception.", ex.Message);
+        }
+
+        [Fact]
+        public async void CheckForUnexpectedException()
+        {
+            ConditionalWait wait = new ConditionalWait();
+            await Assert.ThrowsAsync<NullReferenceException>(() => wait.WaitFor<ArgumentNullException>(() => TestNullRefException(), TimeSpan.FromSeconds(1)));
         }
 
         private void TestVoidDelegate()
@@ -29,6 +36,11 @@ namespace IntelliTect.TestTools.Selenate.Tests
         private bool TestReturnDelegate()
         {
             throw new Exception();
+        }
+
+        private void TestNullRefException()
+        {
+            throw new NullReferenceException();
         }
     }
 }
