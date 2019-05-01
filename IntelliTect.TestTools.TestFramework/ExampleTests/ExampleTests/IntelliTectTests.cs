2d07c576ebbc8245c7f675142800759110d3d56b
Michael Curn <mike.curn@IntelliTect.com>
2019-05-01 16:21:26 -0700

    Stubbing out functional example

--- IntelliTect.TestTools.TestFramework/ExampleTests/ExampleTests/IntelliTectTests.cs
+++ IntelliTect.TestTools.TestFramework/ExampleTests/ExampleTests/IntelliTectTests.cs
@@ -1,5 +1,7 @@
 using ExampleTests.TestBlocks;
 using IntelliTect.TestTools.TestFramework;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Xunit;
 
@@ -7,46 +9,14 @@ namespace ExampleTests
 {
     public class IntelliTectTests
     {
-        private void Test(MethodInfo method)
-        {
-
-        }
-
         [Fact]
         public void Test1()
         {
-
-            // Ideal scenario...
-            //bool r1 = testing.TestBlock1();
-            //testing.TestBlock2(false, r1);
-
-
-            TestBlockGroup testing = new TestBlockGroup();
-
-            Test(testing.TestBlock1);
-
             var expectedResult = new TestBlock2ExpectedValues { ExpectedResult = true };
+
+            // Is there a better way to do this to show the inputs and outputs of test blocks?
+
             TestBuilder builder = new TestBuilder();
-            //builder.AddTestBlock(testing.TestBlock1);
-            //builder.AddTestBlock(
-            //    () => { return testing.TestBlock1(); },
-            //    () => {
-            //        testing.TestBlock2(new TestBlock2ExpectedValues { ExpectedResult = true }, new TestBlock2ActualValues { ActualResult = r1 });
-            //        return false; }
-            //).ExecuteTestByDelegate();
-
-            //builder
-            //    .AddTestBlock(testing.TestBlock1)
-            //    .AddTestBlock(() => { testing.TestBlock2(expectedResult, actualResult); return false; })
-            //    .ExecuteTestByDelegate();
-
-            //builder
-            //    .AddTestBlock(() => {
-            //        r1 = testing.TestBlock1(); })
-            //    .AddTestBlock(() => {
-            //        testing.TestBlock2(new TestBlock2ExpectedValues { ExpectedResult = true }, new TestBlock2ActualValues { ActualResult = r1 }); })
-            //    .ExecuteTestByDelegate();
-
             builder
                 .AddTestBlock<TestBlocks.TestBlock1>()
                 .AddData(expectedResult)
--- IntelliTect.TestTools.TestFramework/IntelliTect.TestTools.TestFramework/TestBuilder.cs
+++ IntelliTect.TestTools.TestFramework/IntelliTect.TestTools.TestFramework/TestBuilder.cs
@@ -9,37 +9,6 @@ namespace IntelliTect.TestTools.TestFramework
 {
     public class TestBuilder
     {
-
-        //public TestBuilder AddTestBlock(Action method)
-        //{
-        //    TestBlocks2.Add(method);
-        //    return this;
-        //}
-
-        //public TestBuilder AddTestBlock(MethodInfo method)
-        //{
-        //    TestBlocks2.Add(method);
-        //    return this;
-        //}
-
-        //public TestBuilder AddTestBlock<T, TResult>(Func<T, TResult> method)
-        //{
-        //    TestBlocks2.Add(method);
-        //    return this;
-        //}
-
-        //public TestBuilder AddTestBlock<T1, T2, TResult>(Func<T1, T2, TResult> method)
-        //{
-        //    TestBlocks2.Add(method);
-        //    return this;
-        //}
-
-        //public TestBuilder AddTestBlock<TResult>(params Func<TResult>[] methods)
-        //{
-        //    TestBlocks2.AddRange(methods);
-        //    return this;
-        //}
-
         public TestBuilder AddTestBlock<T>()
         {
             TestBlockTypes.Add(typeof(T));
@@ -63,36 +32,6 @@ namespace IntelliTect.TestTools.TestFramework
             Data.AddRange(data);
             return this;
         }
-
-        public TestBuilder AddTestBlock(MethodInfo method)
-        {
-            method.Invoke(method.DeclaringType, null);
-            return this;
-        }
-
-        //public void ExecuteTestByDelegate()
-        //{
-        //    //foreach (Action tb in TestBlocks)
-        //    //{
-        //    //    Debug.WriteLine($"Starting test block {tb.Method.Name}");
-        //    //    Stopwatch sw = new Stopwatch();
-        //    //    var test = tb.Method.GetParameters();
-        //    //    sw.Start();
-        //    //    tb();
-        //    //    sw.Stop();
-        //    //    Debug.WriteLine($"Time for test block to execute: {sw.Elapsed}");
-        //    //}
-        //    foreach (Delegate tb in TestBlocks2)
-        //    {
-        //        Debug.WriteLine($"Starting test block {tb.Method.Name}");
-        //        Stopwatch sw = new Stopwatch();
-        //        var test = tb.Method.GetParameters();
-        //        sw.Start();
-        //        object result = tb.Method.Invoke(tb.Target, new object[] { });
-        //        sw.Stop();
-        //        Debug.WriteLine($"Time for test block to execute: {sw.Elapsed}");
-        //    }
-        //}
 
         public void ExecuteTestBlock()
         {
@@ -123,11 +62,6 @@ namespace IntelliTect.TestTools.TestFramework
                 {
                     Data.Add(result);
                 }
-                //var parameters = tb..GetParameters();
-                ////foreach (var p in parameters)
-                //{
-
-                //}
             }
         }
 
