2c1c1bce1c7ca4282d2c35d557b2fbdef79c0083
Cameron J Osborn <cameron.osborn@intellitect.com>
2020-09-15 15:40:20 -0700

    Add sample showing advantage of TestTools.Data (#92)
    * Add sample showing advantage of TestTools.Data
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

---
+++ Samples/IntelliTect.TestTools.Data.Sample/SampleDataProject/MyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleDataProject
+{
+    public class MyService
+    {
+        private SampleDbContext _Db;
+
+        public MyService(SampleDbContext db)
+        {
+            _Db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        // This service is supposed to return a collection of BlogPosts with the person property populated.
+        // However, since it lacks a .Include(x => x.Person) in a production setting with a properly scoped DbContext,
+        // the Person property will not be populated.
+        public async Task<ICollection<BlogPost>> GetBlogsByPerson(string name)
+        {
+            return await _Db.BlogPosts
+                .Where(x => x.Person.Name == name)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Samples/IntelliTect.TestTools.Data.Sample/SampleDataProject/SampleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SampleDataProject
+{
+    public class SampleDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
+        {
+        }
+    }
+}
